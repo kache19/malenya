8501b48843af3365b8f49d7e7a66ca4b7bc09e05
kache09 <kache09@gmail.com>
2025-12-15 23:05:38 +0300

    added url changes

--- backend_php/api/auth/login.php
+++ backend_php/api/auth/login.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../../../config/database.php';
-require_once '../../../utils/auth.php';
+require_once '../../config/database.php';
+require_once '../../utils/auth.php';
 
 global $pdo;
 
